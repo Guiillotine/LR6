cd2ae8b1b0e3dfdea5d6be1a2cae3d7db4966bea
Elena <kozlova03@inbox.ru>
2021-10-25 21:55:47 +0700

    Программа на C#

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LR6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cow cow = new Cow(); Food food = new Food(); List_cow list_cow1 = new List_cow();
+            List_cow list_cow2 = new List_cow();//Для стада коров
+            cow.Set_udder(0.41, 0.42, 0.43, 0.44); //Заполнение параметров вымени внутри кода
+            food.Set("Шашлык"); food.Set("Греча");
+            food.Set();
+            for (int i = 0; i < 3; i++)
+            {
+                list_cow1.Add(food);
+                list_cow1.Print_list();
+            }
+
+            list_cow2.Herd(6); //Создание стада на 6 коров
+            list_cow2.Print_list();
+
+            Console.Write("\n Объем вымени для коровы № 1 списка 1: " + list_cow1.Volume_udder_c(1));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ food.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LR6
+{
+    class Food
+    {
+        public void Set(String name)
+        {
+            this.arr_food[num] = name;
+            this.num++;
+        }
+        public void Set() //Функция инициализации элементов данных
+        {
+            Console.Write("\n Добавление нового корма\n");
+            Console.Write("\n Введите название корма: ");
+            arr_food[num] = Console.ReadLine();
+            num++;
+        }
+        public int Num_sp() //Возвращает количество кормов в списке
+        {
+            return (num);
+        }
+        public String Food_name(int k) //Возвращает название корма
+        {
+            return (arr_food[k]);
+        }
+        private String[] arr_food = new String[10];
+        private int num = 0;
+    }
+}
